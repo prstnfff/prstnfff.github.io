584e10e551eda2631c21bfcab5e30e17fc0952e8
preston <prstnwhite@gmail.com>
2022-12-31 21:43:40 -0800

    adding index file to root of repo

--- js/footer.js
+++ js/footer.js
@@ -6,9 +6,9 @@ class Footer extends HTMLElement {
     connectedCallback(){
         this.innerHTML= 
             `<footer>
-                    <a href="mailto:prstnwhite@gmail.com" target="_blank"><img class='icon' src='icons/mail.png' alt="prstnwhite@gmail.com"></a> 
-                    <a href="https://www.linkedin.com/in/preston-white-b19623101/" target="_blank"><img class='icon' src='icons/linkedin.png' alt="link to LinkedIn"> </a>
-                    <a href="https://github.com/prstnfff" target="_blank"><img class='icon' src='.icons/github.png' alt='link to GitHub'></a>
+                    <a href="mailto:prstnwhite@gmail.com" target="_blank"><img class='icon' src='../icons/mail.png' alt="prstnwhite@gmail.com"></a> 
+                    <a href="https://www.linkedin.com/in/preston-white-b19623101/" target="_blank"><img class='icon' src='../icons/linkedin.png' alt="link to LinkedIn"> </a>
+                    <a href="https://github.com/prstnfff" target="_blank"><img class='icon' src='../icons/github.png' alt='link to GitHub'></a>
             </footer>`
     }
 }
